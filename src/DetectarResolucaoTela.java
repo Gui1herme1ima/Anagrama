972cf5a05660bc803d2d12ce720b962a58fbd8c3
Lima <130940527+Gui1herme1ima@users.noreply.github.com>
2023-10-27 09:50:25 -0300

    Update DetectarResolucaoTela.java
    Adicionado comentários no código

--- src/DetectarResolucaoTela.java
+++ src/DetectarResolucaoTela.java
@@ -4,13 +4,15 @@ import java.awt.Toolkit;
 public class DetectarResolucaoTela {
 
     public int largura, altura;
+    //No código main (AnagramaApp), há um código que pega a resolução da tela.
+    //As variáveis de largura e altura da tela, é enviada como parâmetro.
     DetectarResolucaoTela(int width, int height) {
 
         this.largura = width;
         this.altura = height;
 
     }
-
+    //Sem uso, por enquanto.
     public int LayoutPanelSuperior() {
         if (largura == 1920 && altura == 1080) {
         return 100;
@@ -22,6 +24,8 @@ public class DetectarResolucaoTela {
             return 100;
         }
     }
+    //Em diferentes resoluções de tela, o código desorganiza. Para organizar, selecionei as resoluções mais comuns.
+    //O valor retornado, define o espaçamento horizontal do painel no AnagramaAPP, de acordo com a resolução da tela.
     public int LayoutPanelLetraPalavra() {
         if (largura == 1920 && altura == 1080) {
             return 50;
@@ -33,6 +37,8 @@ public class DetectarResolucaoTela {
             return 100;
         }
     }
+    //Em diferentes resoluções de tela, o código desorganiza. Para organizar, selecionei as resoluções mais comuns.
+    //O valor retornado, define o espaçamento horizontal do painel no AnagramaAPP, de acordo com a resolução da tela.
     public int LayoutPanelLinha() {
         if (largura == 1920 && altura == 1080) {
             return 100;
@@ -44,6 +50,7 @@ public class DetectarResolucaoTela {
             return 100;
         }
     }
+    //Sem uso, por enquanto.
     public int LayoutPanelInferior() {
         if (largura == 1920 && altura == 1080) {
             return 100;
@@ -55,7 +62,10 @@ public class DetectarResolucaoTela {
             return 100;
         }
     }
-
+    //Em diferentes resoluções de tela, o código desorganiza. Para organizar, selecionei as resoluções mais comuns.
+    //O valor retornado, é uma gambiarrakkkk, mas esses espaços em brancos, foram testsados manualmente, de modo que cada letra ficasse em cima da linha, de acordo com cada resolução.
+    //Cada número a frente do "LayoutPalavra", é a quantidade de letras em cada palavra. Quanto mais palavraas, menor os espaços em branco, pois ele vai aproximando mais da borda da janela.
+    //Obs: falta configurar a resolução 1920x1080
     public String LayoutPalavra2(){
         if (largura == 1920 && altura == 1080) {
             return "";
